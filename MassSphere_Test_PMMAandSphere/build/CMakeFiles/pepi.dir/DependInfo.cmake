a9c311a3a298b7e74345cfda37d822164e5966d9
SofiaManzueta <manzueta@insa-toulouse.fr>
2025-09-14 15:42:42 +0200

    First commit

---
+++ MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/pepi.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/pepi.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiActionInitialization.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiActionInitialization.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiController.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiController.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiControllerMessenger.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiControllerMessenger.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiDetectorConstruction.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiDetectorConstruction.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiDetectorMessenger.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiDetectorMessenger.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiEventAction.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiEventAction.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiPSPixiRad.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiPSPixiRad.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiPhysicsList.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiPhysicsList.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiPhysicsListMessenger.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiPhysicsListMessenger.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiPhysicsXrayRefraction.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiPhysicsXrayRefraction.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiPrimaryGeneratorAction.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiPrimaryGeneratorAction.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiRun.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiRun.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiRunAction.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiRunAction.cc.o"
+  "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/src/PepiXrayRefraction.cc" "/home/smanzuetaf/MassSphere_Test_PMMAandSphere/build/CMakeFiles/pepi.dir/src/PepiXrayRefraction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/HEP/GATE/geant4.10.06.p01-install/include/Geant4"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtCore"
+  "/usr/share/qt4/mkspecs/default"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
